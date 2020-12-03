d1499ee241954e2dccd1615f96dfba0bfff6d606
kakashizc <359005442@qq.com>
2020-12-03 11:52:25 +0800

    55

--- application/build.php
+++ application/build.php
@@ -13,7 +13,7 @@
 return [
 // 生成应用公共文件
     '__file__' => [],
-    // 定义demo模块的自动生成 （按照实际定义的文件名生成）
+    // 定义demo模块的自动生成 （按照实际定义的文件名生成!）
     'demo'     => [
         '__file__'   => ['common.php'],
         '__dir__'    => ['behavior', 'controller', 'model', 'view'],
