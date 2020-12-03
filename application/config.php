7ee1e67558438e5a01a94fd8a682e49bd3f5da17
kakashizc <359005442@qq.com>
2020-12-03 08:44:01 +0800

    test

--- application/config.php
+++ application/config.php
@@ -148,7 +148,7 @@ return [
     // 异常页面的模板文件
     'exception_tmpl'         => APP_PATH . 'common' . DS . 'view' . DS . 'tpl' . DS . 'think_exception.tpl',
     // 错误显示信息,非调试模式有效
-    'error_message'          => '你所浏览的页面暂时无法访问!',
+    'error_message'          => '你所浏览的页面暂时无法访问!!!',
     // 显示错误信息
     'show_error_msg'         => false,
     // 异常处理handle类 留空使用 \think\exception\Handle
