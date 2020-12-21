77943ba95c4907bfec6d1c5852385954b50456d0
kakashizc <359005442@qq.com>
2020-12-21 10:06:02 +0800

    创建余额记录菜单

---
+++ application/admin/lang/zh-cn/feed_brush.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'Brush_id'          => '刷手id',
+    'Money'             => '金额',
+    'Status'            => '类型',
+    'Status 1'          => '增加余额',
+    'Status 2'          => '减少余额',
+    'Ctime'             => '操作时间',
+    'Brush.mobile'      => '登录手机号',
+    'Brush.indent_name' => '身份证名字'
+];
