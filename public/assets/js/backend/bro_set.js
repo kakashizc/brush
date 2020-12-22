c1f83646abd9aebed9afd3fa5b84d050ae31b28f
kakashizc <359005442@qq.com>
2020-12-22 11:36:50 +0800

    佣金范围表add

---
+++ public/assets/js/backend/bro_set.js
@@ -0,0 +1,52 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'bro_set/index' + location.search,
+                    add_url: 'bro_set/add',
+                    edit_url: 'bro_set/edit',
+                    del_url: 'bro_set/del',
+                    multi_url: 'bro_set/multi',
+                    table: 'bro_set',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'low', title: __('Low'), operate:'BETWEEN'},
+                        {field: 'high', title: __('High'), operate:'BETWEEN'},
+                        {field: 'bro', title: __('Bro'), operate:'BETWEEN'},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
