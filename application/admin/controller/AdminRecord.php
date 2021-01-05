11c2a463a50670d6053c005411eca3a6ea7c2027
kakashizc <359005442@qq.com>
2021-01-05 15:10:06 +0800

    商家只看到自己的财务记录

--- application/admin/controller/AdminRecord.php
+++ application/admin/controller/AdminRecord.php
@@ -30,6 +30,46 @@ class AdminRecord extends Backend
      * 因此在当前控制器中可不用编写增删改查的代码,除非需要自己控制这部分逻辑
      * 需要将application/admin/library/traits/Backend.php中对应的方法复制到当前控制器,然后进行修改
      */
+    /**
+     * 查看
+     */
+    public function index()
+    {
+        //设置过滤方法
+        $this->request->filter(['strip_tags']);
+        if ($this->request->isAjax()) {
+            //如果发送的来源是Selectpage，则转发到Selectpage
+            if ($this->request->request('keyField')) {
+                return $this->selectpage();
+            }
+            list($where, $sort, $order, $offset, $limit) = $this->buildparams();
+            //如果不是总管理员,那么只获取自己商户的订单
+            $where1 = "";
+            $admin_id = $this->auth->id;
+            //$group_id = $this->auth->getRoleid($admin_id)??'';//获取当前角色的管理组id
+            if ($admin_id != 1){
+                $where1 = ["admin_id"=>$admin_id];
+            }
+            $total = $this->model
+                ->where($where)
+                ->where($where1)
+                ->order($sort, $order)
+                ->count();
+
+            $list = $this->model
+                ->where($where)
+                ->where($where1)
+                ->order($sort, $order)
+                ->limit($offset, $limit)
+                ->select();
+
+            $list = collection($list)->toArray();
+            $result = array("total" => $total, "rows" => $list);
+
+            return json($result);
+        }
+        return $this->view->fetch();
+    }
     
 
 }
